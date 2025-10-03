fd1dd74ddc83239f6f430824849dbd6a20cae8cf
YU-WEI,HSU <blackfire106.1231@gmail.com>
2025-10-03 16:27:19 +0800

    Implement timer interrupt
    Implemented periodic timer interrupt functionality.
    The current interrupt handler is provided by the XC8 wrapper.
     However, since it automatically handles register saving and
    restoring, a custom interrupt handler will be needed in the
    future to enable preemptive scheduling.
    Signed-off-by: YU-WEI,HSU <blackfire106.1231@gmail.com>

--- include/hal.h
+++ include/hal.h
@@ -5,7 +5,15 @@
 #pragma config LVP = OFF
 #pragma config PBADEN = OFF
 
+// #define EXTERNAL_CLOCK
+
+#ifdef EXTERNAL_CLOCK
 #define _XTAL_FREQ 40000000ULL
+#define ONE_SEC 39063
+#else
+#define _XTAL_FREQ 4000000ULL
+#define ONE_SEC 62500
+#endif
 
 void uart_init(void);
 
