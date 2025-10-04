cb7b3fae47ed6bbeeb6af6b0bd31859de427355a
YU-WEI,HSU <blackfire106.1231@gmail.com>
2025-10-05 04:08:27 +0800

    Remove duplicate T0CS setting
    Signed-off-by: YU-WEI,HSU <blackfire106.1231@gmail.com>

--- src/interrupt.c
+++ src/interrupt.c
@@ -4,13 +4,11 @@
 inline void timer0_init(void)
 {
 #ifdef EXTERNAL_CLOCK  // 40M
-    T0CONbits.T0CS = 0;
     T0CONbits.T0PS = 0b111;  // 256
 #else                        // 4M
-    T0CONbits.T0CS = 0;
     T0CONbits.T0PS = 0b011;  // 16
 #endif
-
+    T0CONbits.T0CS = 0;
     T0CONbits.T08BIT = 0;  // 16 bits timer
     T0CONbits.PSA = 0;
     INTCONbits.TMR0IF = 0;
