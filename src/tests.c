06b00d50126940a4883d7afc0a8dc59b5d10a338
Chisheng Chen <johnny1001s000602@gmail.com>
2025-12-14 21:47:00 +0800

    Update memory test to fit the actual address range
    There are two 512KB SRAM in this project, ranging 0 to 0x2000 and
    0x4000 to 0x6000.

--- src/tests.c
+++ src/tests.c
@@ -16,13 +16,20 @@ void extern_memory_test(void)
     extern_memory_write(0x5d00, a);
     for (int i = 0; i < 64; i++)
         a[i] = 0;
-    for (int i = 0; i < 1; i++) {
-        extern_memory_read(0x5d00, a);
-        for (int i = 0; i < 64; i++)
-            printf("%x: %x\n", i, a[i]);
+
+    extern_memory_read(0x5d00, a);
+    for (int i = 0; i < 64; i++) {
+        printf("%x: %x\n", i, a[i]);
     }
+
     rand_seed = 0xdead;
-    for (uint16_t addr = 0b100000000000000; addr <= 0b101111111111111; addr++) {
+    for (uint16_t addr = 0; addr < 0x2000; addr++) {
+        for (int i = 0; i < 64; i++)
+            a[i] = yee_rand();
+        extern_memory_write(addr, a);
+    }
+
+    for (uint16_t addr = 0x4000; addr < 0x6000; addr++) {
         for (int i = 0; i < 64; i++)
             a[i] = yee_rand();
         extern_memory_write(addr, a);
@@ -33,7 +40,20 @@ void extern_memory_test(void)
     rand_seed = 0xdead;
     int fail = 0;
 
-    for (uint16_t addr = 0b100000000000000; addr <= 0b101111111111111; addr++) {
+    for (uint16_t addr = 0; addr < 0x2000; addr++) {
+        extern_memory_read(addr, a);
+        if ((addr & 0xFF) == 0) {
+            printf("addr: 0x%x\n", addr);
+        }
+        for (int i = 0; i < 64; i++) {
+            if (a[i] != yee_rand()) {
+                fail = 1;
+                printf("fail: %x, addr = 0x%x\n", i, addr);
+                goto done;
+            }
+        }
+    }
+    for (uint16_t addr = 0x4000; addr < 0x6000; addr++) {
         extern_memory_read(addr, a);
         if ((addr & 0xFF) == 0) {
             printf("addr: 0x%x\n", addr);
