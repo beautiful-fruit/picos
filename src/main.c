fc7f4695d9dbc7d8f8eb9aefef4f88cbbf0b4611
bir <brian930922@gmail.com>
2025-12-17 04:41:47 +0800

    replace uart_putchar with usr_uart_put_char

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <libc.h>
 #include <schedule.h>
 #include <tests.h>
+#include <usr_libc.h>
 
 #define test_add(arg1, arg2, output) \
     do {                             \
@@ -31,28 +32,20 @@ void __attribute__((naked)) test_add_impl(void)
 
 void __attribute__((naked)) task1(void)
 {
-    lock();
-    uart_putchar('0' + ((run_task_info >> 4) & 0x3));
-    uart_putchar(':');
-    unlock();
-    lock();
-    uart_putchar('x');
-    uart_putchar('\r');
-    uart_putchar('\n');
-    unlock();
+    while (1);
     exit();
 }
 
 void __attribute__((naked)) task2(void)
 {
     lock();
-    uart_putchar('0' + ((run_task_info >> 4) & 0x3));
-    uart_putchar(':');
+    usr_uart_put_char('0' + ((run_task_info >> 4) & 0x3));
+    usr_uart_put_char(':');
     unlock();
     lock();
-    uart_putchar('2');
-    uart_putchar('\r');
-    uart_putchar('\n');
+    usr_uart_put_char('2');
+    usr_uart_put_char('\r');
+    usr_uart_put_char('\n');
     unlock();
     exit();
 }
@@ -62,7 +55,7 @@ void __attribute__((naked)) task3(void)
     while (1) {
         int_wait_queue_push(0);
         lock();
-        uart_putchar('3');
+        usr_uart_put_char('3');
         unlock();
     }
     exit();
@@ -77,18 +70,19 @@ void __attribute__((naked)) task4(void)
         lock();
         
         
-        uart_putchar('$');
-        uart_putchar(' ');
+        usr_uart_put_char('$');
+        usr_uart_put_char(' ');
         
         
         cmd_index = 0;
         while(1) {
-            char ch = uart_getchar();
+            char ch;
+            usr_uart_get_char(ch);
             
             
             if (ch == '\r' || ch == '\n') {
-                uart_putchar('\r');
-                uart_putchar('\n');
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
                 cmd_buffer[cmd_index] = '\0'; 
                 break;
             }
@@ -96,14 +90,14 @@ void __attribute__((naked)) task4(void)
             else if (ch == '\b' || ch == 0x7F) {
                 if (cmd_index > 0) {
                     cmd_index--;
-                    uart_putchar('\b');
-                    uart_putchar(' ');
-                    uart_putchar('\b');
+                    usr_uart_put_char('\b');
+                    usr_uart_put_char(' ');
+                    usr_uart_put_char('\b');
                 }
             }
             
             else if (ch >= 32 && ch <= 126 && cmd_index < 31) {
-                uart_putchar(ch);  
+                usr_uart_put_char(ch);  
                 cmd_buffer[cmd_index++] = ch;
             }
         }
@@ -121,168 +115,213 @@ void __attribute__((naked)) task4(void)
                 if (cmd_index > 4 && cmd_buffer[4] == ' ') {
                     
                     for (uint8_t i = 5; i < cmd_index; i++) {
-                        uart_putchar(cmd_buffer[i]);
+                        usr_uart_put_char(cmd_buffer[i]);
                     }
-                    uart_putchar('\r');
-                    uart_putchar('\n');
+                    usr_uart_put_char('\r');
+                    usr_uart_put_char('\n');
                 }
                 
                 else if (cmd_index == 4) {
-                    
-                    uart_putchar('U');
-                    uart_putchar('s');
-                    uart_putchar('a');
-                    uart_putchar('g');
-                    uart_putchar('e');
-                    uart_putchar(':');
-                    uart_putchar(' ');
-                    uart_putchar('e');
-                    uart_putchar('c');
-                    uart_putchar('h');
-                    uart_putchar('o');
-                    uart_putchar(' ');
-                    uart_putchar('<');
-                    uart_putchar('t');
-                    uart_putchar('e');
-                    uart_putchar('x');
-                    uart_putchar('t');
-                    uart_putchar('>');
-                    uart_putchar('\r');
-                    uart_putchar('\n');
+                    /*
+                    usr_uart_put_char('U');
+                    usr_uart_put_char('s');
+                    usr_uart_put_char('a');
+                    usr_uart_put_char('g');
+                    usr_uart_put_char('e');
+                    usr_uart_put_char(':');
+                    usr_uart_put_char(' ');
+                    usr_uart_put_char('e');
+                    usr_uart_put_char('c');
+                    usr_uart_put_char('h');
+                    usr_uart_put_char('o');
+                    usr_uart_put_char(' ');
+                    usr_uart_put_char('<');
+                    usr_uart_put_char('t');
+                    usr_uart_put_char('e');
+                    usr_uart_put_char('x');
+                    usr_uart_put_char('t');
+                    usr_uart_put_char('>');
+                    usr_uart_put_char('\r');
+                    usr_uart_put_char('\n');
+                    */
+                   char* str = "Usage: echo <text>\r\n";
+                   for(uint8_t i=0;i<20;i++)
+                   {usr_uart_put_char(str[i]);}
                 }
             }
             
             else if (cmd_index == 2 && 
                      cmd_buffer[0] == 'p' && 
                      cmd_buffer[1] == 's') {
-                
-                uart_putchar('P');
-                uart_putchar('I');
-                uart_putchar('D');
-                uart_putchar(' ');
-                uart_putchar('S');
-                uart_putchar('t');
-                uart_putchar('a');
-                uart_putchar('t');
-                uart_putchar('e');
-                uart_putchar(' ');
-                uart_putchar('S');
-                uart_putchar('t');
-                uart_putchar('a');
-                uart_putchar('c');
-                uart_putchar('k');
-                uart_putchar('\r');
-                uart_putchar('\n');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('-');
-                uart_putchar('\r');
-                uart_putchar('\n');
-                
+                /*
+                usr_uart_put_char('P');
+                usr_uart_put_char('I');
+                usr_uart_put_char('D');
+                usr_uart_put_char(' ');
+                usr_uart_put_char('S');
+                usr_uart_put_char('t');
+                usr_uart_put_char('a');
+                usr_uart_put_char('t');
+                usr_uart_put_char('e');
+                usr_uart_put_char(' ');
+                usr_uart_put_char('S');
+                usr_uart_put_char('t');
+                usr_uart_put_char('a');
+                usr_uart_put_char('c');
+                usr_uart_put_char('k');
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('-');
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
+                */
+                char* str="PID State Stack\r\n------------------\r\n";
+                for(uint8_t i=0;i<38;i++)
+                {
+                    usr_uart_put_char(str[i]);
+                }
                 
                 for (uint8_t i = 0; i < 4; i++) {
                     if (run_task_info & (1 << i)) {
                         
-                        uart_putchar('0' + i);
-                        uart_putchar(' ');
-                        uart_putchar(' ');
+                        usr_uart_put_char('0' + i);
+                        usr_uart_put_char(' ');
+                        usr_uart_put_char(' ');
                         
                         
                         if ((run_task_info >> 4) & 0x3 == i) {
-                            uart_putchar('R');  // Running
+                            usr_uart_put_char('R');  // Running
                         } else if (wait_task_info & (1 << i)) {
-                            uart_putchar('W');  // Waiting
+                            usr_uart_put_char('W');  // Waiting
                         } else {
-                            uart_putchar('S');  // Sleeping/Ready
+                            usr_uart_put_char('S');  // Sleeping/Ready
                         }
                         
                         
                         if (run_task_info & RUN_TASK_EXIT) {
-                            uart_putchar('X');  
+                            usr_uart_put_char('X');  
                         } else {
-                            uart_putchar(' ');
+                            usr_uart_put_char(' ');
                         }
                         
-                        uart_putchar(' ');
-                        uart_putchar(' ');
+                        usr_uart_put_char(' ');
+                        usr_uart_put_char(' ');
                         
                         
                         if (run_task[i].stack_info.stack_size > 0) {
-                            uart_putchar('0' + run_task[i].stack_info.stack_start);
-                            uart_putchar('-');
-                            uart_putchar('0' + (run_task[i].stack_info.stack_start + 
+                            usr_uart_put_char('0' + run_task[i].stack_info.stack_start);
+                            usr_uart_put_char('-');
+                            usr_uart_put_char('0' + (run_task[i].stack_info.stack_start + 
                                                run_task[i].stack_info.stack_size - 1));
                         } else {
-                            uart_putchar('N');
-                            uart_putchar('/');
-                            uart_putchar('A');
+                            usr_uart_put_char('N');
+                            usr_uart_put_char('/');
+                            usr_uart_put_char('A');
                         }
                         
-                        uart_putchar('\r');
-                        uart_putchar('\n');
+                        usr_uart_put_char('\r');
+                        usr_uart_put_char('\n');
                     }
                 }
                 
-                
-                uart_putchar('\r');
-                uart_putchar('\n');
-                uart_putchar('S');
-                uart_putchar('t');
-                uart_putchar('a');
-                uart_putchar('c');
-                uart_putchar('k');
-                uart_putchar(' ');
-                uart_putchar('u');
-                uart_putchar('s');
-                uart_putchar('e');
-                uart_putchar(':');
-                uart_putchar(' ');
+                /*
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
+                usr_uart_put_char('S');
+                usr_uart_put_char('t');
+                usr_uart_put_char('a');
+                usr_uart_put_char('c');
+                usr_uart_put_char('k');
+                usr_uart_put_char(' ');
+                usr_uart_put_char('u');
+                usr_uart_put_char('s');
+                usr_uart_put_char('e');
+                usr_uart_put_char(':');
+                usr_uart_put_char(' ');
+                */
+                str = "\r\nStack use: ";
+                for(uint8_t i=0;i<14;i++)
+                {
+                    usr_uart_put_char(str[i]);
+                }
                 
                 for (uint8_t i = 0; i < 4; i++) {
                     if (stack_status.use & (1 << i)) {
-                        uart_putchar('1');
+                        usr_uart_put_char('1');
                     } else {
-                        uart_putchar('0');
+                        usr_uart_put_char('0');
                     }
                 }
-                uart_putchar('\r');
-                uart_putchar('\n');
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
                 
+            }
+            else if (cmd_index == 7 && 
+                    cmd_buffer[0] == 'f' && 
+                    cmd_buffer[1] == 's' &&
+                    cmd_buffer[2] == 's' &&
+                    cmd_buffer[3] == 't' &&
+                    cmd_buffer[4] == 'a' &&
+                    cmd_buffer[5] == 'r' &&
+                    cmd_buffer[6] == 't' &&
+                    )
+            {
+
+            }
+
+            else if (cmd_index == 5 && 
+                    cmd_buffer[0] == 'f' && 
+                    cmd_buffer[1] == 's' &&
+                    cmd_buffer[2] == 'e' &&
+                    cmd_buffer[3] == 'n' &&
+                    cmd_buffer[4] == 'd' &&
+                    )
+            {
+
             }
             
             else {
-                uart_putchar('?');
-                uart_putchar(' ');
+                usr_uart_put_char('?');
+                usr_uart_put_char(' ');
                 for (uint8_t i = 0; i < cmd_index; i++) {
-                    uart_putchar(cmd_buffer[i]);
+                    usr_uart_put_char(cmd_buffer[i]);
                 }
-                uart_putchar('\r');
-                uart_putchar('\n');
-                uart_putchar('T');
-                uart_putchar('r');
-                uart_putchar('y');
-                uart_putchar(':');
-                uart_putchar(' ');
-                uart_putchar('e');
-                uart_putchar('c');
-                uart_putchar('h');
-                uart_putchar('o');
-                uart_putchar(',');
-                uart_putchar(' ');
-                uart_putchar('p');
-                uart_putchar('s');
-                uart_putchar('\r');
-                uart_putchar('\n');
+                /*
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
+                usr_uart_put_char('T');
+                usr_uart_put_char('r');
+                usr_uart_put_char('y');
+                usr_uart_put_char(':');
+                usr_uart_put_char(' ');
+                usr_uart_put_char('e');
+                usr_uart_put_char('c');
+                usr_uart_put_char('h');
+                usr_uart_put_char('o');
+                usr_uart_put_char(',');
+                usr_uart_put_char(' ');
+                usr_uart_put_char('p');
+                usr_uart_put_char('s');
+                usr_uart_put_char('\r');
+                usr_uart_put_char('\n');
+                */
+               char* str = "\r\nTry: echo, ps\r\n";
+               for(uint8_t i=0;i<18;i++)
+               {
+                    usr_uart_put_char(str[i]);
+               }
             }
         }
         
@@ -300,33 +339,17 @@ void main(void)
     uart_init();
     //dma_init();
     extern_memory_init();
-    //ch375_init();
+    ch375_init();
     __delay_ms(3000);
-
+    INTCONbits.GIE = 1;
 
     ADCON1 = 0xF;
     timer0_init();
 
-    init_scheduler();
-    /*
-    create_process(&task3, 10);  // this will fail to create process
-    create_process(&task1, 2);
-    create_process(&task2, 2);
-    create_process(&task1, 1);
-    create_process(&task1, 2);
-    create_process(&task2, 0);
-    create_process(&task2, 0);
-
-    create_process(&task3, 0);
-    create_process(&task4, 0);
-    
-    create_process(&shell, 2);
-    create_process(&shell, 1);
-    create_process(&shell, 0);
-    */
-    
+    init_scheduler();    
     
     create_process(&task4, 2);
+    create_process(&task1, 0);
     
     start_schedule();
     
